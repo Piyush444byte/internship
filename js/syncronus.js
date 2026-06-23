2b4fb4d31292fdee93737f565e8b9a508416c3e5
Admin <admin@example.com>
2026-06-23 12:14:48 +0530

    new update

--- js/syncronus.js
+++ js/syncronus.js
@@ -1,9 +1,37 @@
-function wait(){ 
-    setTimeout(() =>{
-        console.log("5 seconds have passed");
-  },5000);
-}
+//function wait(){ 
+//     setTimeout(() =>{
+//         console.log("timeout Executed");
+//   },0);
 
-console.log("hello");
-wait();
-console.log("world");
+// var id= setInterval(()=>{
+//   console.log("2 sec passed");
+// },2000);
+
+
+// setTimeout(()=>{
+//     clearInterval(id);
+//     console.log("code stopped");
+// },5000);
+// }
+// console.log("hello");
+// wait();
+// console.log("world");
+
+
+// console.log("random tasks");
+// for(let i = 0; i<10; i++){
+//     console.log(i);
+//}
+
+let count = 0;
+const Intervalid = setInterval(()=>{
+    count++;
+    console.log("interval :", count);
+    if(count == 3){
+        clearInterval(Intervalid);
+    }
+},1000);
+
+setTimeout(()=>{
+    console.log("timeout");
+},2500)
