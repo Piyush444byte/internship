0e842f53620e42ac009287a5f2fa07732cd5ab09
Admin <admin@example.com>
2026-06-25 15:50:05 +0530

    working on async-await.js-part1

--- js/async-await.js
+++ js/async-await.js
@@ -1,20 +1,78 @@
 // ASYNC AWAIT
 
 
-function getdata(){
- return new Promise((resolve,reject)=>{
-    resolve("promise resolved");
- });
-}
+// function getdata(){
+//  return new Promise((resolve,reject)=>{
+//     resolve("promise resolved");
+//  });
+// }
 
 
 // getdata().then((msg)=>{
 //     console.log(msg);
 // });
 
-async function handleprom() {
-   const val =  await getdata();
-   console.log(val);
+// async function handleprom() {
+//    const val =  await getdata();
+//    console.log(val);
+// }
+ 
+// handleprom();
+
+
+
+
+// function getdata(){
+//  return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+      
+//     resolve("promise resolved");
+   
+//     },3000);
+//  });
+// }
+
+// async function handle() {
+//     console.log("hello");
+//     const val=await getdata();
+//     console.log(val);
+//     console.log("world");
+
+//     const val2 = await getdata();
+//     console.log(val2);
+//     console.log("world2");
+// };
+
+
+// handle();
+
+
+
+
+function getdata(){
+ return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      
+    resolve("promise resolved");
+   
+    },3000);
+ });
 }
 
-handleprom();
+async function handle() {
+    console.log("hello");
+    const val=await getdata();
+    console.log(val);
+    console.log("world");
+
+    const val2 = await getdata();
+    console.log(val2);
+    console.log("world2");
+};
+
+
+handle();
+
+
+
+
