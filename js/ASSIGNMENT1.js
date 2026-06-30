b0e74fcdde896c33902bb99b44d30471d8f87d96
Admin <admin@example.com>
2026-06-30 15:46:37 +0530

    working on ASSIGNMENT1.js-part3

--- js/ASSIGNMENT1.js
+++ js/ASSIGNMENT1.js
@@ -1,29 +1,73 @@
 
-class Employee {
-  constructor(name, salary) {
-    this.name = name;
-    this.salary = salary;
+// class Employee {
+//   constructor(name, salary) {
+//     this.name = name;
+//     this.salary = salary;
+//   }
+
+//   work() {
+//     console.log(`${this.name} is working`);
+//   }
+// }
+
+
+// class Developer extends Employee {
+//   constructor(name, salary, programmingLanguage) {
+//     super(name, salary);
+//     this.programmingLanguage = programmingLanguage;
+//   }
+
+//   writeCode() {
+//     console.log(`${this.name} is coding in ${this.programmingLanguage}`);
+//   }
+// }
+
+// const developer = new Developer("Piyush", 60000, "JavaScript");
+
+// developer.work();
+// developer.writeCode();
+// developer.name();
+
+
+
+
+
+
+// ShoppingCart class
+class ShoppingCart {
+  constructor() {
+    this.items = [];
   }
 
-  work() {
-    console.log(`${this.name} is working`);
+  // to  add  item
+  addItem(itemName, price) {
+    this.items.push({
+      name: itemName,
+      price: price
+    });
+
+    console.log(`Added ${itemName} to cart`);
+  }
+
+   calculateTotal() {
+    let total = 0;
+
+    for (let item of this.items) {
+      total += item.price;
+    }
+
+    console.log(`Total Price: $${total}`);
+    return total;
   }
 }
 
 
-class Developer extends Employee {
-  constructor(name, salary, programmingLanguage) {
-    super(name, salary);
-    this.programmingLanguage = programmingLanguage;
-  }
+const cart = new ShoppingCart();
 
-  writeCode() {
-    console.log(`${this.name} is coding in ${this.programmingLanguage}`);
-  }
-}
+//  items
+cart.addItem("Laptop", 1200);
+cart.addItem("Mouse", 25);
+cart.addItem("Keyboard", 80);
 
-const developer = new Developer("Piyush", 60000, "JavaScript");
 
-developer.work();
-developer.writeCode();
-developer.name();
+cart.calculateTotal();
