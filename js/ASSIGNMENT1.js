d8d870401bb2febc56db6bf04c82f9bb08ab21a6
Admin <admin@example.com>
2026-06-30 15:06:34 +0530

    working on ASSIGNMENT1.js-part2

--- js/ASSIGNMENT1.js
+++ js/ASSIGNMENT1.js
@@ -1,1 +1,29 @@
-create
+
+class Employee {
+  constructor(name, salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  work() {
+    console.log(`${this.name} is working`);
+  }
+}
+
+
+class Developer extends Employee {
+  constructor(name, salary, programmingLanguage) {
+    super(name, salary);
+    this.programmingLanguage = programmingLanguage;
+  }
+
+  writeCode() {
+    console.log(`${this.name} is coding in ${this.programmingLanguage}`);
+  }
+}
+
+const developer = new Developer("Piyush", 60000, "JavaScript");
+
+developer.work();
+developer.writeCode();
+developer.name();
