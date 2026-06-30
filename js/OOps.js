657c4a4e6997adacc669c8eeff729b4a6378d0a4
Admin <admin@example.com>
2026-06-30 12:24:50 +0530

    working on OOps.js-part2

--- js/OOps.js
+++ js/OOps.js
@@ -1,3 +1,24 @@
+// const Student1={
+//     name:"Piyush",
+//     attendance: 0,
+
+//     isPresent:function(){
+//         this.attendance++;
+//         console.log(
+//             `${this.name} is present. total attendance = ${this.attendance}`
+//         );
+    
+//     },
+// };
+
+// Student1.isPresent();
+
+class Sudent{
+    constructor(name,attendance){
+        this.name = name;
+        this.attendance = attendance
+    }
+}
 const Student1={
     name:"Piyush",
     attendance: 0,
