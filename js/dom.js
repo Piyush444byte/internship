44d583de5a73ec56e285bf68152a71b42285e436
Admin <admin@example.com>
2026-07-01 14:57:27 +0530

    working on dom.html-part2

--- js/dom.js
+++ js/dom.js
@@ -2,3 +2,9 @@ console.log("DOM manipulation script loaded.");
 
 const h = document. getElementById('heading');
 console.log(h);
+
+const p = document.getElementsByClassName('movie');
+console.log(p);
+
+const h2= document.querySelector('h');
+console.log(h);
