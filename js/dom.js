0b39a0589390c389cc2965de24601d792f719802
Admin <admin@example.com>
2026-07-01 15:04:22 +0530

    1-july-2026

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,12 @@
 console.log("DOM manipulation script loaded.");
 
-const h = document. getElementById('heading');
+const h = document. getElementById('heading'); //first tag
 console.log(h);
 
-const p = document.getElementsByClassName('movie');
+const p = document.getElementsByClassName('movie'); //first tag
 console.log(p);
 
 const h2= document.querySelector('h');
 console.log(h);
+const movie= document.querySelectorAll('.movie');
+console.log(movie);
